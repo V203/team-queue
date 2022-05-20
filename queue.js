04338038807b660d56bd730207cc07a1d6f936db
v203 <vuyisamr203@gmail.com>
2022-05-20 15:01:36 +0200

    rev-I

--- queue.js
+++ queue.js
@@ -1,6 +1,7 @@
 let Node = require("./node");
 
 module.exports =  ()=>{
+   
     
     // var arr_queue = [12,54,67,87,9,10] 
 
@@ -21,7 +22,14 @@ module.exports =  ()=>{
         
     }
 
-    let enqueue = (val)=>{
+    let enqueue = (val)=>{ 
+        // let node1 = new Node(val);
+        // while(node1.next){
+        //     node1.next
+
+        // }
+
+        
         console.log(`The value ${val} has been placed ${arr_queue.push(val)}th position at the back of the queue.`)
 
     }
